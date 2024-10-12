6911efc75fa7e0073c684cbca2d4200eb0949ea0
Alessandro <longoa02@gmail.com>
2024-10-13 01:13:22 +0200

    first release

--- src/routes/(app)/game/+page.svelte
+++ src/routes/(app)/game/+page.svelte
@@ -130,13 +130,30 @@
         </button>
     </div>
 
-    <div class="absolute {!info ? 'pointer-events-none' : ''} z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-72">
-        <div class="{info ? 'opacity-100' : 'opacity-0 translate-y-4'} transition-all duration-200 flex flex-col justify-center items-center text-center { $theme == 0 ? 'bg-white border-black text-black' : 'bg-neutral-900 border-white text-white' } p-4 rounded-lg border-2">
-            <p class="text-lg mb-2">
-                To win the game you need to place a queen on each row, column and color.
-            </p>
+    <div class="absolute {!info ? 'pointer-events-none' : ''} z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80">
+        <div class="{info ? 'opacity-100' : 'opacity-0 translate-y-4'} transition-all duration-200 flex flex-col justify-center items-center text-center { $theme == 0 ? 'bg-white border-black text-black' : 'bg-neutral-900 border-white text-white' } px-6 py-4 rounded-lg border-2">
+            <div class="flex flex-col items-center">
+                <div class="flex flex-row justify-center items-center gap-2 mb-2">
+                    <svelte:component this={ls.Info} size={36} class="p-1 m-auto"/>
+                    <h1 class="text-2xl font-bold my-2 text-center { $theme == 0 ? 'text-black' : 'text-white'}">
+                        How to play
+                    </h1>
+                </div>
+
+                <p class="text-lg mb-4 text-start">
+                    To win the game you need to place a queen 
+                    <img src="/pieces/wQ.svg" alt="white queen" class="size-6 inline-block m-auto"/>
+                    on each row, column and color.
+                </p>
+                <p class="text-lg mb-4 text-start">
+                    Left click on a cell to place a queen
+                    <img src="/pieces/wQ.svg" alt="white queen" class="size-6 inline-block m-auto"/>
+                    , or right click to flag it with a cross
+                    <svelte:component this={ls.X} size={16} class="inline-block m-auto"/>.
+                </p>
+            </div>
             
-            <button on:click={toggleInfo} class="transition-colors duration-200 border-2 rounded-lg px-4 py-2 mb-2 mt-4 text-black
+            <button on:click={toggleInfo} class="transition-colors duration-200 border-2 rounded-lg px-4 py-2 mb-2 mt-2 text-black
                 { $theme == 0 ? 
                     'bg-blue-400 hover:bg-blue-500 border-white text-black' : 
                     'bg-neutral-900 border-blue-500 text-white hover:bg-blue-500/25'
@@ -147,18 +164,13 @@
         </div>
     </div>
 
-    <!-- {#if info || win || closeWindowMessage}
-        <div class="absolute w-screen h-screen m-auto transition-all duration-75 bg-black/50">
-        </div>
-    {/if} -->
-
-    <div class="absolute {!closeWindowMessage ? 'pointer-events-none' : ''} z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-72">
+    <div class="absolute {!closeWindowMessage ? 'pointer-events-none' : ''} z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80">
         <div class="{closeWindowMessage ? 'opacity-100' : 'opacity-0 translate-y-4'} transition-all duration-200 flex flex-col justify-center items-center text-center bg-white p-4 rounded-lg border-2 border-black">
-            <p class="text-lg text-black mb-2">
+            <p class="text-lg text-black mt-2 mb-4">
                 Are you sure you want to exit?
             </p>
             
-            <div class="flex flex-row justify-center items-center gap-2 ">
+            <div class="flex flex-row justify-center items-center gap-4">
                 <button on:click={toggleCloseWindowMessage} class="text-black bg-neutral-200 hover:bg-neutral-300 rounded-lg px-4 py-2">Cancel</button>
                 <button on:click={closeCurrentWindow} class="text-black bg-red-400 hover:bg-red-500 rounded-lg px-4 py-2">Exit</button>
             </div>
@@ -179,73 +191,57 @@
     </div>
 
     {#if board}
-        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-4 m-auto">
-            <Timer bind:seconds={seconds}/>
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div class="flex flex-col justify-center items-center gap-4 m-auto translate-y-8">
+                <Timer bind:seconds={seconds}/>
 
-            <div class="flex flex-row m-auto border-4 border-white">
-                {#each board.cells as row, i}
-                    <div class="flex flex-col">
-                        {#each row as cell, j}
-                            <!-- {@const leftBorder = i === 0 || board.cells[i - 1][j].color != board.cells[i][j].color ? 
-                                $theme == 0 ? 'border-l-4 border-l-black' : 'border-l-2 border-l-white' :
-                                $theme == 0 ? 'border-l border-black' : 'border-l border-white'
-                            }
-                            {@const rightBorder = i === board.cells.length - 1 ? 
-                                $theme == 0 ? 'border-r-4 border-r-black' : 'border-r-2 border-r-white' :
-                                ''
-                            }
-                            {@const topBorder = j === 0 || board.cells[i][j - 1].color != board.cells[i][j].color ? 
-                                $theme == 0 ? 'border-t-4 border-t-black' : 'border-t-2 border-t-white' :
-                                $theme == 0 ? 'border-t border-black' : 'border-t border-white'
-                            }
-                            {@const bottomBorder = j === board.cells[0].length - 1 ? 
-                                $theme == 0 ? 'border-b-4 border-b-black' : 'border-b-2 border-b-white' :
-                                ''
-                            } -->
+                <div class="flex flex-row m-auto border-4 border-white">
+                    {#each board.cells as row, i}
+                        <div class="flex flex-col">
+                            {#each row as cell, j}
+                                {@const leftBorder = i === 0 || board.cells[i - 1][j].color != board.cells[i][j].color ? 'border-l-4 border-l-black' : 'border-l border-black'}
+                                {@const rightBorder = i === board.cells.length - 1 ? 'border-r-4 border-r-black' : ''}
+                                {@const topBorder = j === 0 || board.cells[i][j - 1].color != board.cells[i][j].color ? 'border-t-4 border-t-black' : 'border-t border-black'}
+                                {@const bottomBorder = j === board.cells[0].length - 1 ? 'border-b-4 border-b-black' : ''}
+                                <button 
+                                    class="{n == 5 || n == 6 ? 'size-20' : n == 7 ? 'size-16' : 'size-12'} {cell.color} border-0 {leftBorder} {rightBorder} {topBorder} {bottomBorder} m-auto"
+                                    on:mousedown|preventDefault={(event) => handleMouseDown(event, i, j)}
+                                    on:mouseenter={() => handleMouseEnter(i, j)}
+                                >
+                                    {#if cell.state === 'queen'}
+                                        <img class="{n == 5 || n == 6 ? 'size-20' : n == 7 ? 'size-16' : 'size-12'} p-1" src="/pieces/wQ.svg" alt="white queen"/>
+                                    {:else if cell.state === 'cross'}
+                                        <div class="pointer-events-none">
+                                            <svelte:component this={ls.X} size="50%" strokeWidth="1" class="m-auto"/>
+                                        </div>
+                                    {/if}
+                                </button>
+                            {/each}
+                        </div>
+                    {/each}
+                </div>
 
-                            {@const leftBorder = i === 0 || board.cells[i - 1][j].color != board.cells[i][j].color ? 'border-l-4 border-l-black' : 'border-l border-black'}
-                            {@const rightBorder = i === board.cells.length - 1 ? 'border-r-4 border-r-black' : ''}
-                            {@const topBorder = j === 0 || board.cells[i][j - 1].color != board.cells[i][j].color ? 'border-t-4 border-t-black' : 'border-t border-black'}
-                            {@const bottomBorder = j === board.cells[0].length - 1 ? 'border-b-4 border-b-black' : ''}
-                            <button 
-                                class="{n == 5 || n == 6 ? 'size-16' : 'size-12'} {cell.color} border-0 {leftBorder} {rightBorder} {topBorder} {bottomBorder} m-auto"
-                                on:mousedown|preventDefault={(event) => handleMouseDown(event, i, j)}
-                                on:mouseenter={() => handleMouseEnter(i, j)}
-                            >
-                                {#if cell.state === 'queen'}
-                                    <img class="{n == 5 || n == 6 ? 'size-16' : 'size-12'} p-1" src="/pieces/wQ.svg" alt="white queen"/>
-                                {:else if cell.state === 'cross'}
-                                    <div class="pointer-events-none">
-                                        <svelte:component this={ls.X} size="50%" strokeWidth="1" class="m-auto"/>
-                                    </div>
-                                {/if}
-                            </button>
-                        {/each}
-                    </div>
-                {/each}
-            </div>
+                <div class="flex flex-row justify-center items-center gap-8">
+                    <button on:click={resetGrid} class="flex flex-row justify-center items-center gap-2 py-1 ps-1 pe-3 transition-colors rounded-lg border border-white
+                        { $theme == 0 ? 
+                            'text-black bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-100 shadow-black/25' : 
+                            'text-white bg-neutral-800 hover:bg-neutral-900 active:bg-neutral-700 shadow-white/25'
+                        }"    
+                    >
+                        <svelte:component this={ls.RotateCcw} size={36} class="p-1 m-auto"/>
+                        Reset
+                    </button>
 
-
-            <div class="flex flex-row justify-center items-center gap-8">
-                <button on:click={resetGrid} class="flex flex-row justify-center items-center gap-2 py-1 ps-1 pe-3 transition-colors rounded-lg border border-white
-                    { $theme == 0 ? 
-                        'text-black bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-100 shadow-black/25' : 
-                        'text-white bg-neutral-800 hover:bg-neutral-900 active:bg-neutral-700 shadow-white/25'
-                    }"    
-                >
-                    <svelte:component this={ls.RotateCcw} size={36} class="p-1 m-auto"/>
-                    Reset
-                </button>
-
-                <button on:click={toggleInfo} class="flex flex-row justify-center items-center gap-2 py-1 ps-1 pe-3 transition-colors rounded-lg border border-white
-                    { $theme == 0 ? 
-                        'text-black bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-100 shadow-black/25' : 
-                        'text-white bg-neutral-800 hover:bg-neutral-900 active:bg-neutral-700 shadow-white/25 '
-                    }"    
-                >
-                    <svelte:component this={ls.Info} size={36} class="p-1 m-auto"/>
-                    How to play
-                </button>
+                    <button on:click={toggleInfo} class="flex flex-row justify-center items-center gap-2 py-1 ps-1 pe-3 transition-colors rounded-lg border border-white
+                        { $theme == 0 ? 
+                            'text-black bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-100 shadow-black/25' : 
+                            'text-white bg-neutral-800 hover:bg-neutral-900 active:bg-neutral-700 shadow-white/25 '
+                        }"    
+                    >
+                        <svelte:component this={ls.Info} size={36} class="p-1 m-auto"/>
+                        How to play
+                    </button>
+                </div>
             </div>
         </div>
     {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,42 @@
 <script>
     import { theme } from "$lib/stores";
+    import Toggle from '$lib/components/Toggle.svelte';
+    import * as ls from 'lucide-svelte';
+
+    let closeWindowMessage = false;
+    function toggleCloseWindowMessage() {
+        if (!info)
+            closeWindowMessage = !closeWindowMessage;
+    }
 </script>
 
-<div class="flex flex-col justify-center items-center h-screen">
-    <div class="flex flex-col justify-center items-center mb-16">
+<div class="flex flex-col justify-center items-center h-screen {$theme ? 'bg-neutral-900' : 'bg-white'}">
+    <div class="absolute flex flex-row justify-end items-center gap-8 top-10 right-10">
+        <Toggle icons={[ls.Sun, ls.Moon]} colors={['text-yellow-400', 'text-blue-900']} bind:selected={$theme}/>
+
+        <button on:click={toggleCloseWindowMessage} class="flex flex-row justify-center items-center gap-2 py-1 ps-3 pe-4 transition-colors rounded-lg shadow-sm border-2
+            { $theme == 0 ?
+                'text-black bg-red-400 hover:bg-red-500 active:bg-neutral-100 border-white' :
+                'text-white bg-netutral-800 hover:bg-red-500/25 active:bg-neutral-100 border-red-500'
+            }"
+        >
+            <svelte:component this={ls.LogOut} size={32} class="p-1 m-auto"/>
+            Exit
+        </button>
+    </div>
+
+    <div class="flex flex-col justify-center items-center mb-8">
         <div class="flex flex-row justify-center items-center">
-            <p class="text-6xl">Queen's Game</p>
+            <p class="text-8xl font-medium { $theme == 0 ? 'text-black' : 'text-white'}">Queen's Game</p>
         </div>
     </div>
 
-    <a href='/game' class="bg-red-500 hover:bg-red-600 transition-colors text-white text-lg px-4 py-2 rounded-lg hover:no-underline">
+    <a href='/game' class="transition-colors text-2xl px-8 py-2 rounded-lg hover:no-underline border-2
+        { $theme == 0 ?
+            'text-black bg-red-400 hover:bg-red-500 active:bg-neutral-100 border-white' :
+            'text-white bg-netutral-800 hover:bg-red-500/25 active:bg-neutral-100 border-red-500'
+        }"
+    >
         Play!
     </a>
 </div>
